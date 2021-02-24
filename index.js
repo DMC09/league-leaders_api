91ff041d5c450faef11c8b793bec2a12422101d2
“Dharmin <dharminlive1213@mail.com>
2021-02-23 20:57:12 -0600

    installed node-cron, change variables and added comments

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+// Imports
 const express = require("express");
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const cron = require('node-cron');
+const app = express();
 
+// Variable declartion
 const endpoint = `https://api.sportradar.us/nba/trial/v7/en/seasons/2020/REG/leaders.json?api_key=${process.env.API_KEY}`
 let port = process.env.PORT || 8080;
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
 
-const app = express();
+// middleware
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+//setup
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -23,25 +24,18 @@ app.use(function(req, res, next) {
     next();
 });
 
+//main endpoint.
 app.get('/',async (req,res)=>{
 try{
   const response = await axios.get(endpoint);
-  const bosu = await response.data
+  const info = await response.data
     // res.json({ username: 'Flavio' });
-    res.json({ data: bosu });
+    res.json({ data: info });
 
 } catch(e){
   console.log(e);
 }
 });
-// app.get('/',(req,res)=>{
-//   axios.get(endpoint).then(res=>{
-//     console.log(res.data,'this is the Data');
-//   });
-//   // res.send(`This is the base route and the time is ${dateTime}`);
-//   res.json({ username: 'Flavio' });
-//   console.log('this is base route test');
-// });
 
 
 app.listen(port, () => {
